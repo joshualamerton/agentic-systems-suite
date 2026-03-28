246cd0272e3bca24c0c5dff7203438087cf64f62
joshualamerton <137266214+joshualamerton@users.noreply.github.com>
2026-03-28 18:57:37 +0700

    Create amazon.py

---
+++ src/commerce/amazon.py
@@ -0,0 +1,9 @@
+class Amazon:
+    def execute(self, params):
+        return [
+            {
+                "title": "Amazon Sofa",
+                "price": 600,
+                "url": "https://amazon.com/example"
+            }
+        ]
