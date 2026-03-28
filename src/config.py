96fb26780b43f5dfe376fa09f44af338e13bb959
joshualamerton <137266214+joshualamerton@users.noreply.github.com>
2026-03-28 18:55:23 +0700

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+    SHOPIFY_STORE_URL = os.getenv("SHOPIFY_STORE_URL")
+    SHOPIFY_STOREFRONT_TOKEN = os.getenv("SHOPIFY_STOREFRONT_TOKEN")
